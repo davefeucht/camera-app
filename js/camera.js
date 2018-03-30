8cf8d5b43b1d33e0489de224d58bc23ef2893740
Dave Feucht <poetas@gmail.com>
2018-03-30 09:46:32 +0200

    Added page header div, image placeholder, included jQuery, and added code to display the image in the page after it is taken

--- js/camera.js
+++ js/camera.js
@@ -1,3 +1,8 @@
-var takePicture = document.querySelector("#take-picture");
-
-
+$("#take-picture").on("change", function(e) {
+  let data = e.originalEvent.target.files[0];
+  let reader = new FileReader();
+  reader.onload = function(event) {
+    $(".image-div").attr("src", event.target.result);
+    reader.readAsDataUrl(data);
+  };
+});
