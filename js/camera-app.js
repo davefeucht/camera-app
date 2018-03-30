68b71fb46be6a18e36769b04059e49a3e7f4d1fd
Dave Feucht <poetas@gmail.com>
2018-03-30 13:54:23 +0200

    Added camera button image, implemented loading the captured image into the page.

--- js/camera-app.js
+++ js/camera-app.js
@@ -1,8 +1,12 @@
-$("#take-picture").on("change", function(e) {
-  let data = e.originalEvent.target.files[0];
-  let reader = new FileReader();
-  reader.onload = function(event) {
-    $(".image-div").attr("src", event.target.result);
-    reader.readAsDataUrl(data);
-  };
+$(document).ready(function() {
+  $("#take-picture").on("change", function(e) {
+    let data = e.originalEvent.target.files[0];
+    let reader = new FileReader();
+    reader.onload = function(event) {
+      let image = document.createElement("img");
+      image.src = event.target.result;
+      $(".image-div").append(image);
+    };
+    reader.readAsDataURL(data);
+  });
 });
