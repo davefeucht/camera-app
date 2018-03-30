234a81c205364be0794c9db7c39decbcebff9aee
Dave Feucht <poetas@gmail.com>
2018-03-30 15:53:13 +0200

    Remove camera.js from repository after it was renamed to camera-app.js

--- js/camera.js
+++
@@ -1,8 +0,0 @@
-$("#take-picture").on("change", function(e) {
-  let data = e.originalEvent.target.files[0];
-  let reader = new FileReader();
-  reader.onload = function(event) {
-    $(".image-div").attr("src", event.target.result);
-    reader.readAsDataUrl(data);
-  };
-});
