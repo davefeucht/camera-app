79596c13443b4f4533f6b6337204bafac173001f
Dave Feucht <poetas@gmail.com>
2018-04-02 18:59:42 +0200

    Added canvas element to page and modified javascript code to draw image in canvas instead of appending an img tag to the div. Added CSS to initially hide the canvas.

--- js/camera-app.js
+++ js/camera-app.js
@@ -1,16 +1,25 @@
+//When the DOM is fully loaded...
 $(document).ready(function() {
+  //Setup an event handler on change of the image input
   $("#take-picture").on("change", function(e) {
+    //Get a reference to the file
     let data = e.originalEvent.target.files[0];
+    //Set up a FileReader
     let reader = new FileReader();
+    //Set up the onload function of the FileReader
     reader.onload = function(event) {
-      if($(".scaled-image").length > 0) {
-        $(".scaled-image").attr("src", event.target.result);
-      }
-      else {
-        let image = document.createElement("img");
-        image.src = event.target.result;
-        $(".image-div").append(image);
-        $(".image-div img").addClass("scaled-image");
+      //Get the canvas element
+      let image = document.createElement("img");
+      let canvas = document.getElementById("image-canvas");
+      let context = canvas.getContext("2d");
+      let deviceWidth = window.innerWidth;
+
+      canvas.width = deviceWidth - 40;
+      canvas.height = deviceWidth - 40;
+      image.src = event.target.result;
+      image.onload = function() {
+        context.drawImage(image, 0, 0, canvas.width, canvas.height);
+        $("#image-canvas").css("display", "inline");
       }
     };
     reader.readAsDataURL(data);
