96279d510f5582c5097732825f4bb01f45d9326b
Dave Feucht <poetas@gmail.com>
2018-04-16 15:37:28 +0200

    Added correctly rotating the phone image 90 degrees and then basic support for drawing over the image

--- js/camera-app.js
+++ js/camera-app.js
@@ -1,26 +1,124 @@
+function draw(context, positions) {
+  context.beginPath();
+  context.moveTo(positions.prevX, positions.prevY);
+  context.lineTo(positions.currX, positions.currY);
+  context.strokeStyle = "black";
+  context.lineWidth = 2;
+  context.stroke();
+  context.closePath();
+}
+
+function findxy(eventType, positions, canvas, draw_flag, e) {
+  let context = canvas.getContext("2d");
+  let dot_flag = false;
+  if (eventType === "touchstart" || eventType === "mousedown") {
+    positions.prevX = positions.currX;
+    positions.prevY = positions.currY;
+    positions.currX = e.clientX - canvas.offsetLeft;
+    positions.currY = e.clientY - canvas.offsetTop;
+  
+    dot_flag = true;
+    if (dot_flag) {
+      context.beginPath();
+      context.fillStyle = "black";
+      context.fillRect(positions.currX, positions.currY, 2, 2);
+      context.closePath();
+      dot_flag = false;
+    }
+  }
+  if (eventType === "touchmove" || eventType === "mousemove") {
+    if (draw_flag) {
+      positions.prevX = positions.currX;
+      positions.prevY = positions.currY;
+      positions.currX = e.clientX - canvas.offsetLeft;
+      positions.currY = e.clientY - canvas.offsetTop;
+      draw(context, positions);
+    }
+  }
+}
+
+function drawRotated(image, degrees) {
+    let canvas = document.getElementById("image-canvas");
+    let context = canvas.getContext("2d");
+
+    let aspect_ratio = image.width / image.height;
+
+    canvas.width = window.innerWidth - 10;
+    canvas.height = canvas.width * aspect_ratio;
+    
+    context.clearRect(0,0,canvas.width,canvas.height);
+
+    // save the unrotated context of the canvas so we can restore it later
+    // the alternative is to untranslate & unrotate after drawing
+    context.save();
+
+    // move the (0,0) point to the center of the canvas
+    context.translate(canvas.width/2, canvas.height/2);
+
+    // rotate the canvas the specified number of degrees
+    context.rotate(degrees*Math.PI/180);
+
+    /* 
+      Draw the image. Since the context is shifted so (0,0) is in the middle, we need to start
+      drawing from half the width of the canvas to the left and half the height of the canvas
+      up. Since the canvas is rotated, we need draw the image with the x and y sizes swapped. 
+      Currently this only handles a 90 degree rotation.
+    */
+    context.drawImage(image, -canvas.height/2, -canvas.width/2, canvas.height, canvas.width);
+
+    // we’re done with the rotating so restore the unrotated context
+    context.restore();
+}
+
 //When the DOM is fully loaded...
 $(document).ready(function() {
+
+  let image = document.createElement("img");
+  let canvas = document.getElementById("image-canvas");
+  let positions = {currX: 0, currY: 0, prevX: 0, prevY: 0};
+  let clicked = false;
+
+  $("#image-canvas").on("touchstart", function(e) {
+    clicked = true;
+    findxy("touchstart", positions, canvas, clicked, e);
+  });
+  $("#image-canvas").on("mousedown", function(e) {
+    clicked = true;
+    findxy("mousedown", positions, canvas, clicked, e);
+  });
+  $("#image-canvas").on("touchend", function(e) {
+    clicked = false;
+    findxy("touchend", positions, canvas, clicked, e);
+  });
+  $("#image-canvas").on("mouseup", function(e) {
+    clicked = false;
+    findxy("mouseup", positions, canvas, clicked, e);
+  });
+  $("#image-canvas").on("touchmove", function(e) {
+    findxy("touchmove", positions, canvas, clicked, e);
+  });
+  $("#image-canvas").on("mousemove", function(e) {
+    findxy("mousemove", positions, canvas, clicked, e);
+  });
+
   //Setup an event handler on change of the image input
   $("#take-picture").on("change", function(e) {
     //Get a reference to the file
     let data = e.originalEvent.target.files[0];
+
     //Set up a FileReader
     let reader = new FileReader();
+
     //Set up the onload function of the FileReader
     reader.onload = function(event) {
-      //Get the canvas element
-      let image = document.createElement("img");
-      let canvas = document.getElementById("image-canvas");
-      let context = canvas.getContext("2d");
-      let deviceWidth = window.innerWidth;
 
-      canvas.width = deviceWidth - 40;
-      canvas.height = deviceWidth - 40;
+      //set the image source to the result of the FileReader
       image.src = event.target.result;
       image.onload = function() {
-        context.drawImage(image, 0, 0, canvas.width, canvas.height);
+        //context.drawImage(image, 0, 0, canvas.width, canvas.height);
         $("#image-canvas").css("display", "inline");
-      }
+        drawRotated(image, 90);
+      };
     };
     reader.readAsDataURL(data);
   });
